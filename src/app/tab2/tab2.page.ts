37fd693a41fd842d185485cdf3bacd9f4d018ac7
Silvio bis <primoboby@hotmail.com>
2023-03-03 22:19:45 -0300

    update
    terminando

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  pontosTimeA: number = 0;
+  pontosTimeB: number = 0;
+  vitoriasTimeA: number = 0;
+  vitoriasTimeB: number = 0;
 
   constructor() {}
 
+  pontuar(pontos: number, time: string) {
+    if (time === 'timeA') {
+      this.pontosTimeA += pontos;
+    } else if (time === 'timeB') {
+      this.pontosTimeB += pontos;
+    }
+
+    if (this.pontosTimeA >= 12) {
+      this.vitoriasTimeA++;
+      this.resetar();
+    } else if (this.pontosTimeB >= 12) {
+      this.vitoriasTimeB++;
+      this.resetar();
+    }
+  }
+
+  resetar() {
+    this.pontosTimeA = 0;
+    this.pontosTimeB = 0;
+
+  }
 }
